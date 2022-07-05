f8e316bcb972758a46d265313390bfb1f407bb56
zhaqj12 <zhaqj12@naver.com>
2022-07-05 13:44:23 +0900

    Pair with Tuple and tie

--- Piar with Tuple/main.cpp
+++ Piar with Tuple/main.cpp
@@ -1,11 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string a;
+pair<int, int> pi;
+tuple<int, int, int> tl;
+int a, b, c;
 
 int main()
 {
-  cin >> a;
-  cout << "hello world" << endl;
-  cout << a << endl;
+  pi = {1, 2};
+  tl = make_tuple(1, 2, 3);
+  tie(a, b) = pi;
+  cout << a << ":" << b << endl;
+  tie(a, b, c) = tl;
+  cout << a << ":" << b << " : " << c << endl;
+  return 0;
 }
+
+// pair 와 Tuple
+// 두가지 값을 담아야할 때 Pair
+// 세가지 값을 담아야할 때 tuple
+// tie는 2가지 이상 값을 끄집어 낼 때 사용.
